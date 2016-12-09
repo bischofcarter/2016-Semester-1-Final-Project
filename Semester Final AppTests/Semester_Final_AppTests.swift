58e5e09cedb6c059c3cd880f208959aa7b7dad21
Carter Bischof <student@carter-bischof.local>
2016-12-09 10:22:35 -0600

    Initial Commit

---
+++ Semester Final AppTests/Semester_Final_AppTests.swift
@@ -0,0 +1,36 @@
+//
+//  Semester_Final_AppTests.swift
+//  Semester Final AppTests
+//
+//  Created by Carter Bischof on 12/6/16.
+//  Copyright © 2016 Carter Bischof. All rights reserved.
+//
+
+import XCTest
+@testable import Semester_Final_App
+
+class Semester_Final_AppTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
